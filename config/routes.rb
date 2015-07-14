0007e4fe08e6290f284acaada6dbdcabf60d6000
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-13 22:02:13 -0700

    CP33-Assignment - Created new Model and Controller for Advertisement.  Updated routing file.  Updated index and show files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :posts
+
+
+  resources :posts, :advertisements
   get 'about' => 'welcome#about'
-  get 'contact' => 'welcom#contact'
+  get 'contact' => 'welcome#contact'
+  get 'advertisement' =>'posts#advertisement'
+
 
   root to: 'welcome#index'
 end
