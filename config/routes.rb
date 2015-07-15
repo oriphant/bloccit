01816e3f5244f82337683d76c6a1d7baad378941
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-14 20:26:08 -0700

    :w :q  Please enter the commit message for your changes. Lines starting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
 
 
+=begin
+  get 'question/index'
+  get 'question/show'
+  get 'question/new'
+  get 'question/edit'
+  get 'question/create'
+  get 'question/update'
+  get 'question/destroy'
+  get 'question' => 'welcome#question'
+=end
 
-  resources :posts, :advertisements
+  resources :posts, :advertisements, :questions
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
   get 'advertisement' =>'posts#advertisement'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714001502) do
+ActiveRecord::Schema.define(version: 20150714230320) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20150714001502) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
-    t.string   "string"
     t.text     "body"
     t.boolean  "resolved"
     t.datetime "created_at", null: false
