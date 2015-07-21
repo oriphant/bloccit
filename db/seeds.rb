eda08a5290f0363a9c13fdb7857234e097d7d920
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-20 18:11:43 -0700

    CP38 - Linking User to Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,32 @@
- require 'faker'
+require 'faker'
+
+  # Create Users
+    users = User.all
+   5.times do
+     user = User.new(
+       name:     Faker::Name.name,
+       email:    Faker::Internet.email,
+       password: Faker::Lorem.characters(10)
+     )
+     user.skip_confirmation!
+     user.save!
+   end
+ 
+
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
 
  # Create Posts
+
  50.times do
    Post.create!(
+     user: users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -12,6 +36,7 @@
  # Create Comments
  100.times do
    Comment.create!(
+     # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
@@ -42,7 +67,14 @@ end
   )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+    email: 'oriphant@gmail.com',
+    password: 'helloworld'
+  )
 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
