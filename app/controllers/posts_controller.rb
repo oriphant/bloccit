b0d7238813a0e4f9a7dc4def56d5fa0b60fc2f90
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-21 14:47:02 -0700

    CP39-Assignment - Added policy_scope functionality to post_policy.rb and posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    authorize @posts
+     # @posts = Post.all
+     # authorize @posts
+     @posts = policy_scope(Post)
   end
 
   def show
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,23 @@
-  class PostPolicy < ApplicationPolicy
+class PostPolicy < ApplicationPolicy
     def index?
       true
     end
+#~~~~~~~~~~ CP39-Assignment ~~~~~~~~~~~~~~~
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all 
+      else
+        scope.where(:user_id => user.id)
+      end
+    end
   end
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+end
