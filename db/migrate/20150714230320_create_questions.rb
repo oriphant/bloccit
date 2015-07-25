e44896dbb85e57abbd6812c63b2b3db8893d61b0
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-25 10:34:36 -0700

    CP41 - Commented out create_questions in db migrate. Trying to resolve my Heroku db migration issues

--- db/migrate/20150714230320_create_questions.rb
+++ db/migrate/20150714230320_create_questions.rb
@@ -1,12 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
 
-    create_table :questions do |t|
-      t.string :title
-      t.text :body
-      t.boolean :resolved
+    # create_table :questions do |t|
+    #   t.string :title
+    #   t.text :body
+    #   t.boolean :resolved
 
-      t.timestamps null: false
+    #   t.timestamps null: false
     end
   end
 end
