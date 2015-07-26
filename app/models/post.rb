3a9a8a53c25e689c08aa15efeaef9e8463af4502
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-26 01:50:07 -0700

    CP43-Assignment.  Moved mark_down code to model.  Created methods to keep code Private and DRY.

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,4 +23,22 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+
+
+  def markdown_title
+    markdown_to_html title
+  end
+
+  def markdown_body
+    markdown_to_html body
+  end
+
+  private
+  def markdown_to_html(markdown)
+    renderer=Redcarpet::Render::HTML.new
+    extensions={fenced_code_blocks: true}
+    redcarpet=Redcarpet::Markdown.new(renderer,extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 end
