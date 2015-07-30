b9074516e1cc4138d64ee6a313d7b7792c907e9d
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-07-30 09:53:39 -0700

    CP44 - Changes from chapter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+  mount_uploader :avatar, AvatarUploader
 
   def admin?
     role == 'admin'
