ac2c8118b94eb6d1ddd9c0f011738d5f9831624b
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-08-10 13:12:29 -0700

    CP47-Assignement - implemented shallow nesting on Comment, Post and Topic

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,17 @@ Rails.application.routes.draw do
 
   get 'comments/create'
 
-=begin
-  get 'topics/index'
-  get 'topics/new'
-  get 'topics/show'
-  get 'topics/edit'
-  get 'question/index'
-  get 'question/show'
-  get 'question/new'
-  get 'question/edit'
-  get 'question/create'
-  get 'question/update'
-  get 'question/destroy'
-  get 'question' => 'welcome#question'
-=end
-  
   devise_for :users
   resources :users, only: [:update]
   
   resources :topics do
-    resources :posts, except:[:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except:[:index] 
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
  
-  resources :advertisements
  
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
