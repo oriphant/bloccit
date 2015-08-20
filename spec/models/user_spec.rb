692abd521911d9f331d3c369fd495c4c1b0b138f
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-08-19 21:18:29 -0700

    CP51-Assignment - Completed the RSpec tests for user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,11 +30,19 @@ require 'rails_helper'
 describe User do
   include TestFactories
 
+  before do
+    @post = associated_post
+    @user = authenticated_user
+  end
+
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorites.find_by_post_id(@post)).to be nil  
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(post: @post)
+      expect(@user.favorites.find_by_post_id(@post)).to eq(favorite)
     end
   end
 end
