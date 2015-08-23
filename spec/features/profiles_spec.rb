8eecab0e0e2de1634f2d912525f3d7cb5a4b0463
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-08-23 09:51:49 -0700

    CP54-Assignment - Added Spec code to log a user in and test page viewing

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@
  describe "Visiting profiles" do
  
    include TestFactories
+   include Warden::Test::Helpers
+   Warden.test_mode!
  
    before do
      @user = authenticated_user
@@ -13,7 +15,6 @@
    end
  
    describe "not signed in" do
- 
      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
@@ -22,6 +23,19 @@
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
- 
    end
- end
+
+   describe "Visiting own profile - Signed in" do
+    before do
+      login_as(@user, :scope => :user)
+    end
+    it "shows profile" do
+     visit user_path(@user)
+     expect(current_path).to eq(user_path(@user))
+
+     expect( page ).to have_content(@user.name)
+     expect( page ).to have_content(@post.title)
+     expect( page ).to have_content(@comment.body)
+    end
+   end
+end
