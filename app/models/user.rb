0fabd44c4fac2223b2c754ee0932304c2f8f1397
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-08-25 00:46:57 -0700

    CP55 - Added user rankings, popular posts and improved query speed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,4 +52,14 @@ class User < ActiveRecord::Base
       votes.where(post_id: post.id).first
   end
   
+  def self.top_rated
+    self.select('users.*') # Select all attributes fo the user
+        .select('COUNT(DISTINCT comments.id) AS comments_count') # Count the comments made by the user
+        .select('COUNT(DISTINCT posts.id) AS posts_count') # Count the posts made by the user
+        .select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank') # Add the comment count to the post countand label the sum as "rank"
+        .joins(:posts) # Ties the posts table to the users table, via the user_id
+        .joins(:comments) # Ties the comments table tot he users table, via the user_id
+        .group('users.id') #Instructs the database to group the results so that each user will be returned in a distinct row
+        .order('rank DESC') #Instructs the database to order the results in descending order, by the rank that we created in this query.  (Rank = comment count + post count)
+  end
 end
