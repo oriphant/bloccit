287c57dae5df0de4f03b5d5199e55fed9c0ae375
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-08-30 13:45:31 -0700

    CP57 - Not sure why Ajax is not working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,14 @@ class CommentsController < ApplicationController
  
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment.  Please try again"
-      redirect_to [@post.topic, @post]
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end    
   end
 
   def destroy
